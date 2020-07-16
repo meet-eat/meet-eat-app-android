036951351116cf85dd19dc547efff6461bae4aff
Carlos Schmidt <carlosschmidt97@web.de>
2020-07-17 00:28:33 +0200

    Add LoginActivity, MainActivity with layouts
    Also declare LoginActivity the starting activity of the app
    
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

---
+++ Meet-Eat-App/app/src/main/java/meet_eat/app/LoginActivity.java
@@ -0,0 +1,14 @@
+package meet_eat.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
