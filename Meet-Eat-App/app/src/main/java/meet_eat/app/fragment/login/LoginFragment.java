a62fb6e4b212d33a716b9bb0cc8aa9fbfb362728
Carlos Schmidt <carlosschmidt97@web.de>
2020-07-17 13:23:10 +0200

    Add safe args to project and a navigation
    Navigation from LoginFragment to RegisterFragment
    
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

--- Meet-Eat-App/app/src/main/java/meet_eat/app/fragment/login/LoginFragment.java
+++ Meet-Eat-App/app/src/main/java/meet_eat/app/fragment/login/LoginFragment.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import meet_eat.app.R;
 
@@ -17,11 +17,28 @@ import meet_eat.app.R;
  */
 public class LoginFragment extends Fragment {
 
+    View view;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        view = inflater.inflate(R.layout.fragment_login, container, false);
+
+
+        setButtonOnClickListener();
+
+        return view;
     }
+
+    private void setButtonOnClickListener() {
+        view.findViewById(R.id.btRegister).setOnClickListener(event ->
+                Navigation.findNavController(view).navigate(
+                        LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+                )
+        );
+
+    }
+
 
 }
