903d89c88b5cc84367864357b61c98057ad74dd9
Carlos Schmidt <carlosschmidt97@web.de>
2020-07-17 12:06:02 +0200

    Add RegisterFragment with design, LoginFragment
    Add RegisterFragment with design files, add LoginFragment, update nav_graph_login,change app color design
    
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

---
+++ Meet-Eat-App/app/src/main/java/meet_eat/app/fragment/login/LoginFragment.java
@@ -0,0 +1,27 @@
+package meet_eat.app.fragment.login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import meet_eat.app.R;
+
+/**
+ * Repräsentiert die Anmeldeansicht der Applikation.
+ */
+public class LoginFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+}
---
+++ Meet-Eat-App/app/src/main/java/meet_eat/app/fragment/login/RegisterFragment.java
@@ -0,0 +1,24 @@
+package meet_eat.app.fragment.login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import meet_eat.app.R;
+
+/**
+ * Repräsentiert die Registrierungsansicht der Applikation.
+ */
+public class RegisterFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+}
