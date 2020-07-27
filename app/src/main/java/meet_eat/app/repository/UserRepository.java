d98f7a8e8b568b0eab46f36f4601e076777b1dc3
Michael Hirsch <michael98.hirsch@gmail.com>
2020-07-27 16:08:30 +0200

    Implement UserRepository

--- app/src/main/java/meet_eat/app/repository/UserRepository.java
+++ app/src/main/java/meet_eat/app/repository/UserRepository.java
@@ -1,5 +1,51 @@
 package meet_eat.app.repository;
 
-public class UserRepository {
+import meet_eat.data.entity.Tag;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 
+import java.net.URI;
+import java.util.Objects;
+
+import meet_eat.data.Report;
+import meet_eat.data.entity.user.User;
+import meet_eat.data.entity.user.contact.ContactData;
+import meet_eat.data.entity.user.contact.ContactRequest;
+
+public class UserRepository extends EntityRepository<User> {
+
+    private static final String BASE_URL = "/users"; //TODO
+    private static final String URL_RESET_PASSWORD = "/%s/password/reset"; //TODO
+    private static final String ERROR_MESSAGE_NOT_IMPLEMENTED = "Not implemented yet.";
+
+    public UserRepository() {
+        super(BASE_URL);
+    }
+
+    @Override
+    public User addEntity(User entity) throws RequestHandlerException {
+        //No token for registration
+        RequestEntity<User> requestEntity = new RequestEntity<User>(entity, HttpMethod.POST, URI.create(RequestHandler.SERVER_PATH + BASE_URL));
+        return new RequestHandler<User, User>().handle(requestEntity, HttpStatus.CREATED);
+    }
+
+    public Void resetPassword(String email) throws RequestHandlerException {
+        RequestEntity<Void> requestEntity = new RequestEntity<Void>(HttpMethod.POST, URI.create(RequestHandler.SERVER_PATH + BASE_URL + String.format(URL_RESET_PASSWORD, email)));
+        return new RequestHandler<Void, Void>().handle(requestEntity, HttpStatus.ACCEPTED);
+    }
+
+    public void requestContact(ContactRequest contactRequest) {
+        throw new UnsupportedOperationException(ERROR_MESSAGE_NOT_IMPLEMENTED);
+    }
+
+    public void sendContactData(ContactData data) {
+        throw new UnsupportedOperationException(ERROR_MESSAGE_NOT_IMPLEMENTED);
+    }
+
+    public void report(User user, Report report) throws RequestHandlerException {
+        Objects.requireNonNull(user);
+        user.addReport(Objects.requireNonNull(report));
+        updateEntity(user);
+    }
 }
