56368b657aa54323c2ecb4fd1b8e35169a89e038
Michael Hirsch <michael98.hirsch@gmail.com>
2020-07-27 16:08:22 +0200

    Implement TagRepository

--- app/src/main/java/meet_eat/app/repository/TagRepository.java
+++ app/src/main/java/meet_eat/app/repository/TagRepository.java
@@ -1,5 +1,25 @@
 package meet_eat.app.repository;
 
-public class TagRepository {
+import meet_eat.data.entity.Tag;
+import meet_eat.data.entity.Token;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 
+import java.net.URI;
+import java.util.Objects;
+
+public class TagRepository extends EntityRepository<Tag> {
+
+    private static final String BASE_URL = "/tags"; //TODO
+
+    public TagRepository() {
+        super(BASE_URL);
+    }
+
+    public Iterable<Tag> getTags() throws RequestHandlerException {
+        RequestEntity<Void> requestEntity = new RequestEntity<Void>(getTokenHeaders(), HttpMethod.GET,
+                URI.create(RequestHandler.SERVER_PATH + BASE_URL));
+        return new RequestHandler<Void, Iterable<Tag>>().handle(requestEntity, HttpStatus.OK);
+    }
 }
