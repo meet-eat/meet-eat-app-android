5637f5c08ba9557bf7e4c4d739703b50db8e0595
Michael Hirsch <michael98.hirsch@gmail.com>
2020-07-27 16:07:45 +0200

    Add RequestHandlerException

---
+++ app/src/main/java/meet_eat/app/repository/RequestHandlerException.java
@@ -0,0 +1,12 @@
+package meet_eat.app.repository;
+
+public class RequestHandlerException extends Exception {
+
+    public RequestHandlerException() {
+        super();
+    }
+
+    public RequestHandlerException(String message) {
+        super(message);
+    }
+}
