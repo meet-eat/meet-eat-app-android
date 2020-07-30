d789444b7c1f99d8fd38bcc78f286129ffdaec64
Alexander <42893122+xVypar@users.noreply.github.com>
2020-07-30 13:46:32 +0200

    Add missing views
    Add view for profile deletion, report, filter and contact.
    
    Co-Authored-By: carloss97 <18703981+carloss97@users.noreply.github.com>

--- app/src/main/java/meet_eat/app/fragment/main/offer/OfferReportFragment.java
+++ app/src/main/java/meet_eat/app/fragment/main/offer/OfferReportFragment.java
@@ -15,6 +15,6 @@ public class OfferReportFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_offer_report, container, false);
+        return inflater.inflate(R.layout.fragment_report, container, false);
     }
 }
