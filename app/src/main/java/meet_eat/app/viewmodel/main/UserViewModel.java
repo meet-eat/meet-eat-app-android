f7c1e96e13c3c94d714561357c088d03d2ceb64e
Carlos Schmidt <carlosschmidt97@web.de>
2020-07-30 14:13:27 +0200

    Implement UserViewModel functionality
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

--- app/src/main/java/meet_eat/app/viewmodel/main/UserViewModel.java
+++ app/src/main/java/meet_eat/app/viewmodel/main/UserViewModel.java
@@ -1,12 +1,20 @@
 package meet_eat.app.viewmodel.main;
 
+import androidx.lifecycle.ViewModel;
+
+import meet_eat.app.repository.RequestHandlerException;
+import meet_eat.app.repository.Session;
+import meet_eat.app.repository.UserRepository;
 import meet_eat.data.Report;
 import meet_eat.data.entity.user.User;
 
 /**
  * Manages user-related information.
  */
-public class UserViewModel {
+public class UserViewModel extends ViewModel {
+
+    private final UserRepository userRepository = new UserRepository();
+    private final Session session = Session.getInstance();
 
     /**
      * Requests the object of the user currently logged in to the device from the
@@ -15,18 +23,17 @@ public class UserViewModel {
      * @return The current user.
      */
     public User getCurrentUser() {
-        return null;
+        return session.getUser();
     }
 
     /**
-     * TODO rename to update?
      * Sends a user update request to the
      * {@link meet_eat.app.repository.UserRepository UserRepository}.
      *
      * @param editedUser The user that is to be updated.
      */
-    public void edit(User editedUser) {
-
+    public void edit(User editedUser) throws RequestHandlerException {
+        userRepository.updateEntity(editedUser);
     }
 
     /**
@@ -35,8 +42,8 @@ public class UserViewModel {
      * @param user The user that is to be reported.
      * @param report The report.
      */
-    public void report(User user, Report report) {
-
+    public void report(User user, Report report) throws RequestHandlerException {
+        userRepository.report(user, report);
     }
 
     /**
@@ -45,8 +52,8 @@ public class UserViewModel {
      *
      * @param user The user object with an updated subscriber list.
      */
-    public void subscribe(User user) {
-
+    public void subscribe(User user) throws RequestHandlerException {
+        userRepository.updateEntity(user);
     }
 
     /**
@@ -55,7 +62,7 @@ public class UserViewModel {
      *
      * @param user The user object with an updated subscriber list.
      */
-    public void unsubscribe(User user) {
-
+    public void unsubscribe(User user) throws RequestHandlerException {
+        userRepository.updateEntity(user);
     }
 }
