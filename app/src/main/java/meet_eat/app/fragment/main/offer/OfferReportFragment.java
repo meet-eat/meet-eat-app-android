c255f7eddc5bb748070b5e9b25b80f6a5c408a9f
Alexander <42893122+xVypar@users.noreply.github.com>
2020-07-30 19:23:29 +0200

    Resolve merge issue

--- app/src/main/java/meet_eat/app/fragment/main/offer/OfferReportFragment.java
+++ app/src/main/java/meet_eat/app/fragment/main/offer/OfferReportFragment.java
@@ -17,6 +17,6 @@ public class OfferReportFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_report, container, false);
+        return inflater.inflate(R.layout.fragment_offer_report, container, false);
     }
 }
