602f22e5e31c148fac64e850ea1541c707ee283e
Carlos Schmidt <carlosschmidt97@web.de>
2020-07-30 14:13:11 +0200

    Implement RatingViewModel functionality
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

--- app/src/main/java/meet_eat/app/viewmodel/main/RatingViewModel.java
+++ app/src/main/java/meet_eat/app/viewmodel/main/RatingViewModel.java
@@ -1,12 +1,17 @@
 package meet_eat.app.viewmodel.main;
 
+import androidx.lifecycle.ViewModel;
+
+import meet_eat.app.repository.Session;
 import meet_eat.data.entity.user.User;
 import meet_eat.data.entity.user.rating.Rating;
 
 /**
  * Manages rating-related information.
  */
-public class RatingViewModel {
+public class RatingViewModel extends ViewModel {
+
+    private final Session session = Session.getInstance();
 
     /**
      * Requests the object of the user currently logged in to the device from the
@@ -15,7 +20,7 @@ public class RatingViewModel {
      * @return The current user.
      */
     public User getCurrentUser() {
-        return null;
+        return session.getUser();
     }
 
     /**
@@ -25,16 +30,17 @@ public class RatingViewModel {
      * @param rating The new rating.
      */
     public void send(Rating rating) {
-
+        session.getUser().addRating(rating);
     }
 
     /**
      * Sends multiple requests to the
-     *  {@link meet_eat.app.repository.UserRepository UserRepository} to add new ratings.
+     * {@link meet_eat.app.repository.UserRepository UserRepository} to add new ratings.
      *
      * @param ratings The new ratings.
      */
     public void send(Rating... ratings) {
-
+        for (Rating rating : ratings)
+            send(rating);
     }
 }
