e924cd1bb973423b45a23455bbbe9d980964e226
Carlos Schmidt <carlosschmidt97@web.de>
2020-07-31 15:43:08 +0200

    Implement UI functionality for ProfileEdit view
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

--- app/src/main/java/meet_eat/app/fragment/main/profile/ProfileEditFragment.java
+++ app/src/main/java/meet_eat/app/fragment/main/profile/ProfileEditFragment.java
@@ -4,19 +4,123 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
-import meet_eat.app.R;
+import meet_eat.app.databinding.FragmentProfileEditBinding;
+import meet_eat.app.repository.RequestHandlerException;
+import meet_eat.app.viewmodel.main.UserViewModel;
+import meet_eat.data.entity.user.Password;
+import meet_eat.data.entity.user.User;
 
 public class ProfileEditFragment extends Fragment {
+
+    FragmentProfileEditBinding binding;
+    UserViewModel userVM;
+    private String phone;
+    private String oldPasswordString;
+    private String newPasswordString;
+    private String home;
+    private String description;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile_edit, container, false);
+        binding = FragmentProfileEditBinding.inflate(inflater, container, false);
+        binding.setFragment(this);
+        userVM = new ViewModelProvider(this).get(UserViewModel.class);
+
+        setButtonOnClickListener();
+        return binding.getRoot();
+    }
+
+    private void setButtonOnClickListener() {
+        binding.btProfileEditChangePassword.setOnClickListener(event -> changePassword());
+        binding.btProfileEditSave.setOnClickListener(event -> saveProfile());
+        binding.ibtBack.setOnClickListener(event -> goBack());
+    }
+
+    private void changePassword() {
+        if (!Password.isLegalPassword(oldPasswordString) || !Password.isLegalPassword(newPasswordString)) {
+            Toast.makeText(getActivity(), "Altes Passwort falsch oder neues ungültig", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Password oldPassword = Password
+                .createHashedPassword(oldPasswordString);
+        Password newPassword = Password
+                .createHashedPassword(newPasswordString);
+        if (oldPassword.equals(userVM.getCurrentUser().getPassword())) {
+            userVM.getCurrentUser().setPassword(newPassword);
+            Toast.makeText(getActivity(), "Passwort erfolgreich geändert", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getActivity(), "Altes Passwort falsch", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveProfile() {
+        User currentUser = userVM.getCurrentUser();
+        if (phone != null && !phone.isEmpty())
+            currentUser.setPhoneNumber(phone);
+        if (home != null && !home.isEmpty()) {
+            // TODO
+        }
+        if (description != null && !description.isEmpty())
+            currentUser.setDescription(description);
+
+        try {
+            userVM.edit(currentUser);
+        } catch (RequestHandlerException e) {
+            // TODO timeout etc.
+        }
+    }
+
+    private void goBack() {
+        Navigation.findNavController(binding.getRoot()).popBackStack();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getOldPasswordString() {
+        return oldPasswordString;
+    }
+
+    public void setOldPasswordString(String oldPasswordString) {
+        this.oldPasswordString = oldPasswordString;
+    }
+
+    public String getNewPasswordString() {
+        return newPasswordString;
+    }
+
+    public void setNewPasswordString(String newPasswordString) {
+        this.newPasswordString = newPasswordString;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public void setHome(String home) {
+        this.home = home;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
