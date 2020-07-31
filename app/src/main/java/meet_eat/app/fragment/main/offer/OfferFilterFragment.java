823599e6720b6ebdeafd6dbcdade449bd017af96
Carlos Schmidt <carlosschmidt97@web.de>
2020-07-31 21:54:47 +0200

    Add id to offerFilterSort spinner, example in java
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

--- app/src/main/java/meet_eat/app/fragment/main/offer/OfferFilterFragment.java
+++ app/src/main/java/meet_eat/app/fragment/main/offer/OfferFilterFragment.java
@@ -4,19 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 import meet_eat.app.R;
+import meet_eat.app.databinding.FragmentOfferFilterBinding;
 
 public class OfferFilterFragment extends Fragment {
+
+    private FragmentOfferFilterBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_offer_filter, container, false);
+        binding = FragmentOfferFilterBinding.inflate(inflater, container, false);
+        initializeSortSpinner();
+        return binding.getRoot();
     }
+
+
+    private void initializeSortSpinner() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("R.string.appearance");
+        arrayList.add("ANDROID");
+        arrayList.add("C Language");
+        arrayList.add("CPP Language");
+        arrayList.add("Go Language");
+        arrayList.add("AVN SYSTEMS");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(binding.getRoot().getContext(),
+                android.R.layout.simple_spinner_item, arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.sOfferFilterSort.setAdapter(arrayAdapter);
+    }
+
 }
