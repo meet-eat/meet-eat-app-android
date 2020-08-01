fe82afa88636d1537056ff514485246aada5caf8
Alexander <42893122+xVypar@users.noreply.github.com>
2020-08-01 16:51:08 +0200

    Add update and init ui methods and resolve TODOs
    Co-Authored-By: carloss97 <18703981+carloss97@users.noreply.github.com>

--- app/src/main/java/meet_eat/app/fragment/main/offer/OfferDetailedFragment.java
+++ app/src/main/java/meet_eat/app/fragment/main/offer/OfferDetailedFragment.java
@@ -1,5 +1,6 @@
 package meet_eat.app.fragment.main.offer;
 
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,17 +14,29 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.io.IOException;
+
 import meet_eat.app.R;
 import meet_eat.app.databinding.FragmentOfferDetailedBinding;
+import meet_eat.app.repository.RequestHandlerException;
 import meet_eat.app.viewmodel.main.OfferViewModel;
 import meet_eat.data.entity.Offer;
+import meet_eat.data.entity.user.User;
+import meet_eat.data.location.Localizable;
+import meet_eat.data.location.UnlocalizableException;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 public class OfferDetailedFragment extends Fragment {
 
+    private final static String KEY_OFFER = "offer";
+    private final static String KEY_USER = "user";
+
     private FragmentOfferDetailedBinding binding;
     private OfferViewModel offerVM;
-    private Offer offer;
     private NavController navController;
+    private Offer offer;
 
     @Nullable
     @Override
@@ -34,20 +47,18 @@ public class OfferDetailedFragment extends Fragment {
         offerVM = new ViewModelProvider(requireActivity()).get(OfferViewModel.class);
         navController = NavHostFragment.findNavController(this);
 
-        if (getArguments() == null || getArguments().getSerializable("offer") == null) {
-            Toast.makeText(getActivity(), "DEBUG: Offer not given", Toast.LENGTH_SHORT).show();
+        if (getArguments() == null || getArguments().getSerializable(KEY_OFFER) == null) {
+            // TODO remove debug toast
+            Toast.makeText(getActivity(),
+                    "DEBUG OfferDetailedFragment.java -> getArguments", Toast.LENGTH_LONG).show();
             navController.navigateUp();
         } else {
-            offer = (Offer) getArguments().getSerializable("offer");
+            offer = (Offer) getArguments().getSerializable(KEY_OFFER);
         }
 
-        updateUI();
+        initUI();
         setButtonOnClickListener();
         return binding.getRoot();
-    }
-
-    private void updateUI() {
-        // TODO
     }
 
     private void setButtonOnClickListener() {
@@ -58,55 +69,146 @@ public class OfferDetailedFragment extends Fragment {
         binding.btOfferDetailedParticipants.setOnClickListener(event -> navigateToOfferParticipants());
         binding.ivOfferDetailedProfile.setOnClickListener(event -> navigateToProfile());
         binding.tvOfferDetailedUsername.setOnClickListener(event -> navigateToProfile());
-        binding.btOfferDetailedCancel.setOnClickListener(event -> cancelOffer());
+        binding.btOfferDetailedParticipate.setOnClickListener(event -> participateOffer());
         binding.btOfferDetailedContact.setOnClickListener(event -> navigateToOfferContact());
     }
 
     private void navigateToOfferContact() {
         Bundle bundle = new Bundle();
-        bundle.putSerializable("offer", offer);
+        bundle.putSerializable(KEY_OFFER, offer);
         navController.navigate(R.id.offerContactFragment, bundle);
     }
 
-    private void cancelOffer() {
-        offer.removeParticipant(offerVM.getCurrentUser());
-        // TODO toast?
-        updateUI();
+    private void participateOffer() {
+
+        try {
+
+            if (offer.getParticipants().contains(offerVM.getCurrentUser())) {
+                offer.removeParticipant(offerVM.getCurrentUser());
+                offerVM.cancelParticipation(offerVM.getCurrentUser(), offer);
+            } else {
+                offer.addParticipant(offerVM.getCurrentUser());
+                offerVM.participate(offer);
+            }
+
+            updateUI();
+        } catch (RequestHandlerException e) {
+            // TODO resolve error code
+            Toast.makeText(getActivity(),
+                    "DEBUG OfferDetailedFragment.java -> participateOffer(): " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+
     }
 
     private void navigateToProfile() {
         Bundle bundle = new Bundle();
-        bundle.putSerializable("user", offer.getCreator());
+        bundle.putSerializable(KEY_USER, offer.getCreator());
         navController.navigate(R.id.profileFragment, bundle);
     }
 
     private void navigateToOfferParticipants() {
         Bundle bundle = new Bundle();
-        bundle.putSerializable("offer", offer);
+        bundle.putSerializable(KEY_OFFER, offer);
         navController.navigate(R.id.offerParticipantsFragment, bundle);
     }
 
     private void bookmark(View view) {
 
-        if (offerVM.getCurrentUser().getBookmarks().contains(offer)) {
-            offerVM.getCurrentUser().removeBookmark(offer);
-        } else {
-            offerVM.getCurrentUser().addBookmark(offer);
+        try {
+
+            if (offerVM.getCurrentUser().getBookmarks().contains(offer)) {
+                offerVM.getCurrentUser().removeBookmark(offer);
+                offerVM.removeBookmark(offer);
+            } else {
+                offerVM.getCurrentUser().addBookmark(offer);
+                offerVM.addBookmark(offer);
+            }
+
+            updateUI();
+        } catch (RequestHandlerException e) {
+            // TODO resolve error code
+            Toast.makeText(getActivity(),
+                    "DEBUG OfferDetailedFragment.java -> participateOffer(): " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
         }
 
-        // TODO toast?
-        updateUI();
     }
 
     private void navigateToOfferReport() {
         Bundle bundle = new Bundle();
-        bundle.putSerializable("offer", offer);
+        bundle.putSerializable(KEY_OFFER, offer);
         navController.navigate(R.id.offerReportFragment, bundle);
     }
 
     private void navigateToOfferEdit() {
         Bundle bundle = new Bundle();
-        bundle.putSerializable("offer", offer);
+        bundle.putSerializable(KEY_OFFER, offer);
         navController.navigate(R.id.offerEditFragment, bundle);
+    }
+
+    private void initUI() {
+        // TODO offer image
+        binding.tvOfferDetailedTitle.setText(offer.getName());
+        binding.tvOfferDetailedDate.setText(offer.getDateTime().toString());
+        Localizable location = offer.getLocation();
+
+        try {
+            // TODO distance
+            binding.tvOfferDetailedCity.setText(new Geocoder(binding.getRoot().getContext()).getFromLocation(location.getSphericalPosition().getLatitude(),
+                    location.getSphericalPosition().getLongitude(), 1).get(0).getSubAdminArea());
+        } catch (IOException e) {
+            // TODO remove debug toast
+            Toast.makeText(getActivity(),
+                    "DEBUG OfferDetailedFragment.java -> updateUI(): " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        } catch (UnlocalizableException e) {
+            // TODO remove debug toast
+            Toast.makeText(getActivity(),
+                    "DEBUG OfferDetailedFragment.java -> updateUI(): " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        binding.tvOfferDetailedPrice.setText(String.valueOf(offer.getPrice()) + R.string.currency);
+        binding.tvOfferDetailedParticipants.setText(String.valueOf(offer.getMaxParticipants()));
+        // TODO profile image
+        binding.tvOfferDetailedUsername.setText(offer.getCreator().getName());
+        binding.tvOfferDetailedRating.setText(String.valueOf(offer.getCreator().getHostRating()));
+        binding.tvOfferDetailedDescription.setText(offer.getDescription());
+        // TODO tags
+
+        if (offerVM.getCurrentUser().equals(offer.getCreator())) {
+            binding.ibtOfferDetailedReport.setVisibility(INVISIBLE);
+            binding.ibtOfferDetailedReport.setClickable(false);
+            binding.btOfferDetailedParticipate.setVisibility(INVISIBLE);
+            binding.btOfferDetailedParticipate.setClickable(false);
+            binding.btOfferDetailedContact.setVisibility(INVISIBLE);
+            binding.btOfferDetailedContact.setClickable(false);
+            binding.tvOfferDetailedParticipating.setVisibility(INVISIBLE);
+        } else {
+            binding.ibtOfferDetailedEdit.setVisibility(INVISIBLE);
+            binding.ibtOfferDetailedEdit.setClickable(false);
+            binding.btOfferDetailedParticipants.setVisibility(INVISIBLE);
+            binding.btOfferDetailedParticipants.setClickable(false);
+        }
+
+        updateUI();
+    }
+
+    private void updateUI() {
+
+        if (offerVM.getCurrentUser().getBookmarks().contains(offer)) {
+            binding.ibtOfferDetailedBookmark.setColorFilter(R.color.bookmarked);
+        } else {
+            binding.ibtOfferDetailedBookmark.setColorFilter(R.color.symbol);
+        }
+
+        if (offer.getParticipants().contains(offerVM.getCurrentUser())) {
+            binding.btOfferDetailedParticipate.setText(R.string.cancel);
+            binding.tvOfferDetailedParticipating.setVisibility(VISIBLE);
+        } else {
+            binding.btOfferDetailedParticipate.setText(R.string.participate);
+            binding.tvOfferDetailedParticipating.setVisibility(INVISIBLE);
+        }
     }
 }
