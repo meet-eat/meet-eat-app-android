a8165815791cb9cbdf5cf767c48e2d3de6c6289a
Carlos Schmidt <carlosschmidt97@web.de>
2020-08-05 18:07:57 +0200

    Implement subscriber list view logic
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

--- app/src/main/java/meet_eat/app/fragment/main/profile/ProfileSubscribedAdapter.java
+++ app/src/main/java/meet_eat/app/fragment/main/profile/ProfileSubscribedAdapter.java
@@ -1,16 +1,23 @@
 package meet_eat.app.fragment.main.profile;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+import meet_eat.app.R;
 import meet_eat.app.databinding.ItemProfileSubscriptionBinding;
+import meet_eat.app.repository.RequestHandlerException;
 import meet_eat.app.viewmodel.main.UserViewModel;
 import meet_eat.data.entity.user.User;
+
+import static meet_eat.app.fragment.NavigationArgumentKey.USER;
 
 public class ProfileSubscribedAdapter extends RecyclerView.Adapter<ProfileSubscribedAdapter.ViewHolder> {
 
@@ -55,6 +62,32 @@ public class ProfileSubscribedAdapter extends RecyclerView.Adapter<ProfileSubscr
         }
 
         public void setData(User user) {
+            binding.tvOfferSubscriptionUsername.setText(user.getName());
+            binding.tvOfferSubscriptionUsername.setOnClickListener(event -> navigateToProfile(user));
+            binding.ibtOfferSubscriptionRemove.setOnClickListener(event -> unsubscribe(user));
+            binding.ivOfferSubscriptionProfile.setOnClickListener(event -> navigateToProfile(user));
+        }
+
+        private void navigateToProfile(User user) {
+            Bundle bundle = new Bundle();
+            bundle.putSerializable(USER.name(), user);
+            Navigation.findNavController(binding.getRoot()).navigate(R.id.profileFragment, bundle);
+        }
+
+        private void unsubscribe(User user) {
+            int position = currentSubscriptions.indexOf(user);
+            currentSubscriptions.remove(user);
+            notifyItemRemoved(position);
+
+            try {
+                userVM.unsubscribe(user);
+            } catch (RequestHandlerException e) {
+                // TODO resolve error code
+                Toast.makeText(binding.getRoot().getContext(),
+                        "DEBUG ProfileSubscribedAdapter.java -> unsubscribe(): " + e.getMessage(), Toast.LENGTH_LONG)
+                        .show();
+            }
+
         }
     }
 }
