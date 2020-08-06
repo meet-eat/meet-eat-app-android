4c7b314ec4cb2062b21c4b5b6e250739ed110061
Carlos Schmidt <carlosschmidt97@web.de>
2020-08-06 11:57:09 +0200

    Add SettingsViewModel junit test
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

---
+++ app/src/test/java/meet_eat/app/viewmodel/main/SettingsViewModelTest.java
@@ -0,0 +1,84 @@
+package meet_eat.app.viewmodel.main;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import meet_eat.app.repository.RequestHandlerException;
+import meet_eat.app.viewmodel.login.LoginViewModel;
+import meet_eat.app.viewmodel.login.RegisterViewModel;
+import meet_eat.data.entity.user.Email;
+import meet_eat.data.entity.user.Password;
+import meet_eat.data.entity.user.User;
+import meet_eat.data.location.SphericalLocation;
+import meet_eat.data.location.SphericalPosition;
+
+import static org.junit.Assert.assertNotNull;
+
+public class SettingsViewModelTest {
+
+    private static final String registeredEmail = "registered-email@example.com";
+    private static final String password = "ABcd12§$";
+    private static final String username = "Registered User";
+    private static final String phoneNumber = "0123456789";
+    private static final String profileDescription = "JUnit Test User";
+
+    private static SettingsViewModel settingsVM;
+    private static User registeredUser;
+
+    @BeforeClass
+    public static void initialize() throws RequestHandlerException {
+        RegisterViewModel registerVM = new RegisterViewModel();
+        LoginViewModel loginVM = new LoginViewModel();
+        settingsVM = new SettingsViewModel();
+
+        registeredUser =
+                new User(new Email(registeredEmail), Password.createHashedPassword(password), LocalDate.of(2000, 1, 1),
+                        username, phoneNumber, profileDescription, true,
+                        new SphericalLocation(new SphericalPosition(0, 0)));
+        registerVM.register(registeredUser);
+        loginVM.login(registeredEmail, password);
+    }
+
+    @AfterClass
+    public static void cleanUp() throws RequestHandlerException {
+        settingsVM.deleteUser(registeredUser);
+    }
+
+    @Test
+    public void testGetCurrentUser() {
+        assertNotNull(settingsVM.getCurrentUser());
+    }
+
+    // Not yet implemented
+    @Ignore
+    @Test(expected = NullPointerException.class)
+    public void testUpdateNotificationSettingsWithNull() throws RequestHandlerException {
+        settingsVM.updateNotificationSettings(null);
+    }
+
+    // Not yet implemented
+    @Ignore
+    @Test(expected = NullPointerException.class)
+    public void testUpdateDisplaySettings() throws RequestHandlerException {
+        settingsVM.updateDisplaySettings(null);
+    }
+
+    // Conflict with @AfterClass
+    @Ignore
+    @Test
+    public void testLogout() throws RequestHandlerException {
+        settingsVM.logout();
+    }
+
+    // Conflict with @AfterClass
+    @Ignore
+    @Test(expected = NullPointerException.class)
+    public void testDeleteUserWithNull() throws RequestHandlerException {
+        settingsVM.deleteUser(null);
+    }
+}
