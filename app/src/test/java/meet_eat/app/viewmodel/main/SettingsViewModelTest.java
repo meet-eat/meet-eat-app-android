7aa5f4fe62e1fc5a35867960fb52f3ec36534b3e
Carlos Schmidt <carlosschmidt97@web.de>
2020-08-06 13:27:51 +0200

    Optimize imports
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

--- app/src/test/java/meet_eat/app/viewmodel/main/SettingsViewModelTest.java
+++ app/src/test/java/meet_eat/app/viewmodel/main/SettingsViewModelTest.java
@@ -17,7 +17,6 @@ import meet_eat.data.location.SphericalLocation;
 import meet_eat.data.location.SphericalPosition;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 public class SettingsViewModelTest {
 
