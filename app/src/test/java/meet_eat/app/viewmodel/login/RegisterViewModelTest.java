ebe7e37305f46bb5c73003ca24947f31de242df9
Alexander <42893122+xVypar@users.noreply.github.com>
2020-08-06 11:13:53 +0200

    Add JUnit tests for register view model
    Co-Authored-By: carloss97 <18703981+carloss97@users.noreply.github.com>

---
+++ app/src/test/java/meet_eat/app/viewmodel/login/RegisterViewModelTest.java
@@ -0,0 +1,25 @@
+package meet_eat.app.viewmodel.login;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import meet_eat.app.repository.RequestHandlerException;
+
+public class RegisterViewModelTest {
+
+    private static RegisterViewModel registerVM;
+
+    @BeforeClass
+    public static void initialize() {
+        registerVM = new RegisterViewModel();
+    }
+
+    // Registering a new valid user is tested in LoginViewModelTest
+
+    @Test(expected = NullPointerException.class)
+    public void testRegisterWithNullUser() throws RequestHandlerException {
+        registerVM.register(null);
+    }
+}
