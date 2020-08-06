b2ff78aa1f4f4f73ecbf40c1d7c162a7e155c66e
Michael Hirsch <michael98.hirsch@gmail.com>
2020-08-06 20:16:50 +0200

    Add javadoc to TagRepository

--- app/src/main/java/meet_eat/app/repository/TagRepository.java
+++ app/src/main/java/meet_eat/app/repository/TagRepository.java
@@ -1,22 +1,32 @@
 package meet_eat.app.repository;
 
 import meet_eat.data.entity.Tag;
-import meet_eat.data.entity.Token;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 
 import java.net.URI;
-import java.util.Objects;
 
+/**
+ * Represents the administrative unit that controls access and manipulation of tags within a repository.
+ */
 public class TagRepository extends EntityRepository<Tag> {
 
     private static final String BASE_URL = "/tags"; //TODO
 
+    /**
+     * Creates an tag repository.
+     */
     public TagRepository() {
         super(BASE_URL);
     }
 
+    /**
+     * Returns all available tags from the repository.
+     *
+     * @return all available tags
+     * @throws RequestHandlerException if an error occurs when requesting the repository
+     */
     public Iterable<Tag> getTags() throws RequestHandlerException {
         RequestEntity<Void> requestEntity = new RequestEntity<Void>(getTokenHeaders(), HttpMethod.GET,
                 URI.create(RequestHandler.SERVER_PATH + BASE_URL));
