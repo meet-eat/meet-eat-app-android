425cad5f56111dbf73a9dc197bcbec3cb270d4d2
Alexander <42893122+xVypar@users.noreply.github.com>
2020-08-06 11:13:38 +0200

    Add JUnit tests for login view model
    Co-Authored-By: carloss97 <18703981+carloss97@users.noreply.github.com>

--- app/src/test/java/meet_eat/app/viewmodel/login/LoginViewModelTest.java
+++ app/src/test/java/meet_eat/app/viewmodel/login/LoginViewModelTest.java
@@ -1,21 +1,82 @@
 package meet_eat.app.viewmodel.login;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 import meet_eat.app.repository.RequestHandlerException;
+import meet_eat.app.viewmodel.main.SettingsViewModel;
+import meet_eat.data.entity.user.Email;
+import meet_eat.data.entity.user.Password;
+import meet_eat.data.entity.user.User;
+import meet_eat.data.location.SphericalLocation;
+import meet_eat.data.location.SphericalPosition;
 
 public class LoginViewModelTest {
-    @BeforeClass
-    public static void initialize() {}
 
-    @Test
-    public void testLoginWithNull() throws RequestHandlerException {
-        new LoginViewModel().login(null, null);
+    private static final String validRegisteredEmail = "registered-email@example.com";
+    private static final String validUnregisteredEmail = "unregistered-email@example.com";
+    private static final String invalidEmail = "email example";
+    private static final String password = "ABcd12§$";
+    private static final String username = "Registered User";
+    private static final String phoneNumber = "0123456789";
+    private static final String description = "JUnit Test User";
+
+    private static LoginViewModel loginVM;
+    private static RegisterViewModel registerVM;
+    private static SettingsViewModel settingsVM;
+
+    @BeforeClass
+    public static void initialize() {
+        loginVM = new LoginViewModel();
+        registerVM = new RegisterViewModel();
+        settingsVM = new SettingsViewModel();
     }
 
     @Test
-    public void testResetPasswordWithNull() throws RequestHandlerException {
-        new LoginViewModel().resetPassword(null);
+    public void testRegisterLoginDeleteUser() throws RequestHandlerException {
+        User registeredUser = new User(new Email(validRegisteredEmail), Password.createHashedPassword(password),
+                LocalDate.of(2000, 1, 1), username, phoneNumber, description, true,
+                new SphericalLocation(new SphericalPosition(0, 0)));
+        registerVM.register(registeredUser);
+        loginVM.login(validRegisteredEmail, password);
+        settingsVM.deleteUser(registeredUser);
+    }
+
+    @Test(expected = RequestHandlerException.class)
+    public void testLoginWithUnregisteredCredentials() throws RequestHandlerException {
+        loginVM.login(validUnregisteredEmail, password);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLoginWithInvalidEmail() throws RequestHandlerException {
+        loginVM.login(invalidEmail, password);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLoginWithNullEmail() throws RequestHandlerException {
+        loginVM.login(null, password);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLoginWithNullPassword() throws RequestHandlerException {
+        loginVM.login(validUnregisteredEmail, null);
+    }
+
+    @Test(expected = RequestHandlerException.class)
+    public void testResetPasswordWithValidEmail() throws RequestHandlerException {
+        loginVM.resetPassword(validUnregisteredEmail);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testResetPasswordWithInvalidEmail() throws RequestHandlerException {
+        loginVM.resetPassword(invalidEmail);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testResetPasswordWithNullEmail() throws RequestHandlerException {
+        loginVM.resetPassword(null);
     }
 }
