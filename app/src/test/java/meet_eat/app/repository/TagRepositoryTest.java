cb4f29511d6476a651387fa4986b4748577b00ce
Michael Hirsch <michael98.hirsch@gmail.com>
2020-08-08 19:34:55 +0200

    Use EntityRepositoryTest in TagRepositoryTest

--- app/src/test/java/meet_eat/app/repository/TagRepositoryTest.java
+++ app/src/test/java/meet_eat/app/repository/TagRepositoryTest.java
@@ -1,142 +1,36 @@
 package meet_eat.app.repository;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.Objects;
-
-import meet_eat.data.LoginCredential;
 import meet_eat.data.entity.Tag;
-import meet_eat.data.entity.user.Email;
-import meet_eat.data.entity.user.Password;
-import meet_eat.data.entity.user.User;
-import meet_eat.data.location.CityLocation;
-import meet_eat.data.location.Localizable;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+public class TagRepositoryTest extends EntityRepositoryTest<TagRepository, Tag, String> {
 
-public class TagRepositoryTest {
+    private static final Tag TAG_WITH_ID = new Tag("a8s76d9a39nhc7qxqm87z87", "aldkfmldföldmflmdkfiuh");
+    private static final Tag TAG_WITHOUT_ID = new Tag("daonfjdnafjkndfkjnajkfndajdnkf");
 
-    private static User user;
-    private static LoginCredential loginCredential;
-    private static TagRepository tagRepository;
-
-    @BeforeClass
-    public static void setUpClass() throws RequestHandlerException {
-        // Register
-        Email email = new Email("test@example.com");
-        Password password = Password.createHashedPassword("Str0ngPassw0rd!");
-        LocalDate birthDay = LocalDate.of(1998, Month.FEBRUARY, 6);
-        String name = "JUnit Test User";
-        String phoneNumber = "0123456789";
-        String description = "This is my test description";
-        boolean isVerified = false;
-        Localizable location = new CityLocation("Karlsruhe");
-        User testUser = new User(email, password, birthDay, name, phoneNumber, description, isVerified, location);
-        loginCredential = new LoginCredential(email, password);
-        user = new UserRepository().addEntity(testUser);
-        tagRepository = new TagRepository();
+    public TagRepositoryTest() {
+        super(new TagRepository(), TAG_WITH_ID, TAG_WITHOUT_ID);
     }
 
-    @After
-    public void logout() throws RequestHandlerException {
-        Session session = Session.getInstance();
-        if (Objects.nonNull(session.getToken())) {
-            session.logout();
-        }
-    }
+    // Test addEntity
 
-    @AfterClass
-    public static void tearDownClass() throws RequestHandlerException {
-        Session session = Session.getInstance();
-        if (Objects.isNull(session.getToken())) {
-            session.login(loginCredential);
-        }
-        new UserRepository().deleteEntity(user);
-        user = null;
-        loginCredential = null;
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void testAddEntityNotLoggedIn() throws RequestHandlerException {
-        // Assertions
-        assertNull(Session.getInstance().getToken());
-
-        // Execution
-        tagRepository.addEntity(new Tag("test"));
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void testUpdateEntityNotLoggedIn() throws RequestHandlerException {
-        // Assertions
-        assertNull(Session.getInstance().getToken());
-
-        // Execution
-        tagRepository.addEntity(new Tag("test"));
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void testDeleteEntityNotLoggedIn() throws RequestHandlerException {
-        // Assertions
-        assertNull(Session.getInstance().getToken());
-
-        // Execution
-        tagRepository.addEntity(new Tag("test"));
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void testGetEntityByIdNotLoggedIn() throws RequestHandlerException {
-        // Assertions
-        assertNull(Session.getInstance().getToken());
-
-        // Execution
-        tagRepository.addEntity(new Tag("test"));
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testAddEntityWithNull() throws RequestHandlerException {
-        // Assertions
+    /*@Ignore
+    @Test
+    public void testAddEntityValidWithId() throws RequestHandlerException {
         Session.getInstance().login(loginCredential);
         assertNotNull(Session.getInstance().getToken());
 
         // Execution
-        tagRepository.addEntity(null);
-    }
+        tagRepository.addEntity(tagWitID);
 
-    @Test(expected = NullPointerException.class)
-    public void testUpdateEntityWithNull() throws RequestHandlerException {
         // Assertions
-        Session.getInstance().login(loginCredential);
-        assertNotNull(Session.getInstance().getToken());
+        assertEquals(tagWitID, tagRepository.getEntityById(tagWitID.getIdentifier()));
+        tagRepository.deleteEntity(tagWitID);
+    }*/
 
-        // Execution
-        tagRepository.addEntity(null);
-    }
+    // Test updateEntity
 
-    @Test(expected = NullPointerException.class)
-    public void testDeleteEntityWithNull() throws RequestHandlerException {
-        // Assertions
-        Session.getInstance().login(loginCredential);
-        assertNotNull(Session.getInstance().getToken());
+    // Test deleteEntity
 
-        // Execution
-        tagRepository.addEntity(null);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testGetEntityByIdWithNull() throws RequestHandlerException {
-        // Assertions
-        Session.getInstance().login(loginCredential);
-        assertNotNull(Session.getInstance().getToken());
-
-        // Execution
-        tagRepository.addEntity(null);
-    }
-
+    // Test getEntityById
 
 }
