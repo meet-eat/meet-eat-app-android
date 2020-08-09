25c5c8a02d499915cb6604a08e7ae4f5dcd9030a
Michael Hirsch <michael98.hirsch@gmail.com>
2020-08-09 02:44:45 +0200

    Override testAddEntityNotLoggedIn

--- app/src/test/java/meet_eat/app/repository/UserRepositoryTest.java
+++ app/src/test/java/meet_eat/app/repository/UserRepositoryTest.java
@@ -2,14 +2,21 @@ package meet_eat.app.repository;
 
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Set;
 
+import meet_eat.data.LoginCredential;
 import meet_eat.data.Report;
 import meet_eat.data.comparator.OfferComparableField;
 import meet_eat.data.comparator.OfferComparator;
@@ -26,6 +33,10 @@ import meet_eat.data.entity.user.setting.Setting;
 import meet_eat.data.location.CityLocation;
 import meet_eat.data.location.Localizable;
 import meet_eat.data.predicate.OfferPredicate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class UserRepositoryTest extends EntityRepositoryTest<UserRepository, User, String> {
 
@@ -57,7 +68,7 @@ public class UserRepositoryTest extends EntityRepositoryTest<UserRepository, Use
     }
 
     private static User getUserWithoutId() {
-        Email email = new Email("43v8hg9fjg@example.com");
+        Email email = new Email("423v8hg9fjg@example.com");
         Password password = Password.createHashedPassword("Str0ngPassw0rd!");
         LocalDate birthDay = LocalDate.of(1998, Month.OCTOBER, 16);
         String name = "JUnit Test User";
@@ -67,4 +78,33 @@ public class UserRepositoryTest extends EntityRepositoryTest<UserRepository, Use
         Localizable localizable = new CityLocation("Karlsruhe");
         return new User(email, password, birthDay, name, phoneNumber, description, isVerified, localizable);
     }
+
+    @Test
+    @Override
+    public void testAddEntityNotLoggedIn() throws RequestHandlerException {
+        assertNull(Session.getInstance().getToken());
+
+        // Execution
+        User registeredUser = getEntityRepository().addEntity(getEntityWithoutId());
+
+        // Assertions
+        assertNull(getUserWithoutId().getIdentifier());
+        assertNotNull(registeredUser);
+        assertNotNull(registeredUser.getIdentifier());
+        assertEquals(registeredUser.getEmail(), getUserWithoutId().getEmail());
+    }
+
+
+    @BeforeClass
+    public static void setUpClass() throws RequestHandlerException {
+        // Not needed here
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws RequestHandlerException {
+        Session.getInstance().login(new LoginCredential(getUserWithoutId().getEmail(), getUserWithoutId().getPassword()));
+        new UserRepository().deleteEntity(Session.getInstance().getUser());
+    }
+
+
 }
