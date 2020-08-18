8a89d54f246057ab6b0b55c20e2c3347a4739aa6
Carlos Schmidt <carlosschmidt97@web.de>
2020-08-19 00:00:34 +0200

    Optimize imports
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

--- app/src/androidTest/java/meet_eat/app/Scenario1050Test.java
+++ app/src/androidTest/java/meet_eat/app/Scenario1050Test.java
@@ -1,10 +1,5 @@
 package meet_eat.app;
 
-import android.os.SystemClock;
-import android.view.Gravity;
-
-import androidx.test.espresso.contrib.DrawerActions;
-import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -36,8 +31,6 @@ import meet_eat.data.location.SphericalPosition;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
