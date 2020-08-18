1b060732591a494a0f4c1a6ce753bb22ce64f886
Carlos Schmidt <carlosschmidt97@web.de>
2020-08-18 23:13:35 +0200

    Create scenario 1030 espresso test
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

---
+++ app/src/androidTest/java/meet_eat/app/Scenario1030Test.java
@@ -0,0 +1,79 @@
+package meet_eat.app;
+
+import android.os.SystemClock;
+import android.view.Gravity;
+
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.time.LocalDate;
+
+import meet_eat.app.repository.RequestHandlerException;
+import meet_eat.app.viewmodel.login.RegisterViewModel;
+import meet_eat.app.viewmodel.main.SettingsViewModel;
+import meet_eat.data.entity.user.Email;
+import meet_eat.data.entity.user.Password;
+import meet_eat.data.entity.user.User;
+import meet_eat.data.location.SphericalLocation;
+import meet_eat.data.location.SphericalPosition;
+
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class Scenario1030Test {
+    static long timestamp = System.currentTimeMillis();
+    private static final String password = "123##Tester";
+    private ScenarioTestHelper scenarioTestHelper = new ScenarioTestHelper(timestamp, password);
+
+    @Rule
+    public ActivityTestRule<LoginActivity> activityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @BeforeClass
+    public static void initialize() throws RequestHandlerException {
+        User newUser = new User(new Email(timestamp + "@example.com"), Password.createHashedPassword(password),
+                LocalDate.of(2000, 1, 1), "Tester", "0123456789", "Test description", true,
+                new SphericalLocation(new SphericalPosition(0, 0)));
+        new RegisterViewModel().register(newUser);
+    }
+
+    @AfterClass
+    public static void cleanUp() throws RequestHandlerException {
+        new SettingsViewModel().deleteUser(new SettingsViewModel().getCurrentUser());
+        Intents.release();
+    }
+
+    @Test
+    public void testScenario1030() {
+        Intents.init();
+        scenarioTestHelper.login();
+        // [TA 3000]
+        // Open navigation drawer
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed(Gravity.LEFT))).perform(DrawerActions.open());
+        // Navigate to settings
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.drawer_menu_settings));
+        SystemClock.sleep(500);
+        onView(withId(R.id.btSettingsDelete)).perform(click());
+        onView(withId(R.id.etSettingsDeleteProfilePassword)).perform(typeText(password));
+        closeSoftKeyboard();
+        onView(withId(R.id.btSettingsDeleteProfile)).perform(click());
+        // Check if logged out
+        intended(hasComponent(LoginActivity.class.getName()));
+    }
+}
