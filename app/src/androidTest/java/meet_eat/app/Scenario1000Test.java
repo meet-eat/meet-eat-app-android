078b78f601afcd292b105ed4d2d1b7cc9e58ae57
Carlos Schmidt <carlosschmidt97@web.de>
2020-08-19 00:42:37 +0200

    Change offer creation method
    Co-Authored-By: Alexander <42893122+xvypar@users.noreply.github.com>

--- app/src/androidTest/java/meet_eat/app/Scenario1000Test.java
+++ app/src/androidTest/java/meet_eat/app/Scenario1000Test.java
@@ -6,31 +6,81 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Collection;
+import java.util.HashSet;
+
 import meet_eat.app.repository.RequestHandlerException;
+import meet_eat.app.viewmodel.login.LoginViewModel;
+import meet_eat.app.viewmodel.login.RegisterViewModel;
+import meet_eat.app.viewmodel.main.OfferViewModel;
 import meet_eat.app.viewmodel.main.SettingsViewModel;
+import meet_eat.data.entity.Offer;
+import meet_eat.data.entity.user.Email;
+import meet_eat.data.entity.user.Password;
+import meet_eat.data.entity.user.User;
+import meet_eat.data.location.SphericalLocation;
+import meet_eat.data.location.SphericalPosition;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
 
+@Ignore("Cant test on an emulator")
 @RunWith(AndroidJUnit4.class)
 public class Scenario1000Test {
 
+    private static final OfferViewModel offerVM = new OfferViewModel();
+    private static final String password = "123##Tester";
+
     static long timestamp = System.currentTimeMillis();
-    private final String password = "123##Tester";
     private ScenarioTestHelper scenarioTestHelper = new ScenarioTestHelper(timestamp, password);
 
     @Rule
     public ActivityTestRule<LoginActivity> activityTestRule = new ActivityTestRule<>(LoginActivity.class);
 
+    @BeforeClass
+    public static void initialize() throws RequestHandlerException {
+        User newUser = new User(new Email(timestamp + "@example.com"), Password.createHashedPassword(password),
+                LocalDate.of(2000, 1, 1), "Tester", "0123456789", "Test description", true,
+                new SphericalLocation(new SphericalPosition(0, 0)));
+        new RegisterViewModel().register(newUser);
+
+        addForeignOffer();
+    }
+
+    private static void addForeignOffer() throws RequestHandlerException {
+        User newUser = new User(new Email(timestamp + 1 + "@example.com"), Password.createHashedPassword(password),
+                LocalDate.of(2000, 1, 1), "Tester", "0123456789", "Test description", true,
+                new SphericalLocation(new SphericalPosition(0, 0)));
+        new RegisterViewModel().register(newUser);
+
+        // Create an offer for this test
+        new LoginViewModel().login(timestamp + 1 + "@example.com", password);
+        Offer toBeAdded = new Offer(offerVM.getCurrentUser(), new HashSet<>(), "Offer", "offerDescription", 0, 1,
+                LocalDateTime.of(2030, Month.DECEMBER, 31, 23, 59), new SphericalLocation(new SphericalPosition(6, 6)));
+        assertEquals(0, ((Collection<Offer>) offerVM.fetchOffers(offerVM.getCurrentUser())).size());
+        offerVM.add(toBeAdded);
+        new SettingsViewModel().logout();
+    }
+
     @AfterClass
     public static void cleanUp() throws RequestHandlerException {
-        new SettingsViewModel().deleteUser(new SettingsViewModel().getCurrentUser());
         Intents.release();
+        new LoginViewModel().login(timestamp + 1 + "@example.com", password);
+        offerVM.delete(offerVM.fetchOffers(offerVM.getCurrentUser()).iterator().next());
+        new SettingsViewModel().deleteUser(new SettingsViewModel().getCurrentUser());
+        new LoginViewModel().login(timestamp + "@example.com", password);
+        new SettingsViewModel().deleteUser(new SettingsViewModel().getCurrentUser());
     }
 
     @Test
