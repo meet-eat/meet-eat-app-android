92b822de10d3cc53000f4edec7a7f16c62bca420
Alexander <42893122+xVypar@users.noreply.github.com>
2020-08-18 22:53:45 +0200

    Select the starting navigation drawer menu on start
    Co-Authored-By: carloss97 <18703981+carloss97@users.noreply.github.com>

--- app/src/main/java/meet_eat/app/MainActivity.java
+++ app/src/main/java/meet_eat/app/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this::onItemClicked);
+        navigationView.getMenu().getItem(1).setChecked(true);
         ActionBarDrawerToggle toggle =
                 new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,
                         R.string.navigation_drawer_close);
