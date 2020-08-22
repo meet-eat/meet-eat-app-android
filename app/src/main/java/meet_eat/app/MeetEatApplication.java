40ead80a9d5f00c18069c4e222f6e6a7c9046a54
Michael Hirsch <michael98.hirsch@gmail.com>
2020-08-22 22:56:48 +0200

    Add javadoc to MeetEatApplication

--- app/src/main/java/meet_eat/app/MeetEatApplication.java
+++ app/src/main/java/meet_eat/app/MeetEatApplication.java
@@ -3,15 +3,24 @@ package meet_eat.app;
 import android.app.Application;
 import android.content.Context;
 
+/**
+ * Represents the main application class of Meet & Eat.
+ */
 public class MeetEatApplication extends Application {
 
     private static Context context;
 
+    @Override
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
     }
 
+    /**
+     * Returns the {@link Context context} of the application.
+     *
+     * @return the context of the application
+     */
     public static Context getAppContext() {
         return context;
     }
