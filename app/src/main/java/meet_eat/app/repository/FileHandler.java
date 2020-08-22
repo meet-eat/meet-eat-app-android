1a90de4e8dec918d7e5959ebcd78b8b224a339cc
Michael Hirsch <michael98.hirsch@gmail.com>
2020-08-22 22:45:59 +0200

    Add FileHandler class

---
+++ app/src/main/java/meet_eat/app/repository/FileHandler.java
@@ -0,0 +1,84 @@
+package meet_eat.app.repository;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+import meet_eat.app.MeetEatApplication;
+
+/**
+ * Represents the unit for handling files.
+ */
+public final class FileHandler {
+
+    private static final String ERROR_MESSAGE_NULL_CONTEXT = "No accessible application file directory available.";
+    private static final String ERROR_MESSAGE_NOT_DELETED = "File was not deleted due to an error.";
+    private static final Charset CHARSET = StandardCharsets.UTF_8;
+
+    /**
+     * Creates a {@link FileHandler file handler}.
+     */
+    private FileHandler() {
+    }
+
+    /**
+     * Saves a {@link String} to a file.
+     *
+     * @param fileContent The {@link String} to be saved
+     * @param fileName the name of the file to save the content to
+     * @throws IOException if an I/O error occurs
+     */
+    public static void saveStringToFile(String fileContent, String fileName) throws IOException {
+        Context context = MeetEatApplication.getAppContext();
+        if (Objects.isNull(context)) {
+            throw new IOException(ERROR_MESSAGE_NULL_CONTEXT);
+        }
+        Objects.requireNonNull(fileContent);
+        Objects.requireNonNull(fileName);
+        try (FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE)) {
+            fileOutputStream.write(fileContent.getBytes());
+        }
+    }
+
+    /**
+     * Returns a {@link String} read from a file.
+     *
+     * @param fileName The name of the file the {@link String} is read from.
+     * @return  a {@link String} read from the file
+     * @throws IOException if an I/O error occurs
+     */
+    public static String readFileToString(String fileName) throws IOException {
+        Context context = MeetEatApplication.getAppContext();
+        FileInputStream fileInputStream = context.openFileInput(fileName);
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, CHARSET);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
+            String line;
+            while (Objects.nonNull(line = reader.readLine())) {
+                stringBuilder.append(line);
+                stringBuilder.append(System.lineSeparator());
+            }
+            return stringBuilder.toString();
+        }
+    }
+
+    /**
+     * Deletes a file.
+     *
+     * @param fileName the name of the file to be deleted
+     * @throws IOException if an I/O error occurs
+     */
+    public static void deleteFile(String fileName) throws IOException {
+        if (!MeetEatApplication.getAppContext().deleteFile(fileName)) {
+            throw new IOException(ERROR_MESSAGE_NOT_DELETED);
+        }
+    }
+}
